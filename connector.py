1752224d85c4e6817ecd86f8c1e007a99b524979
Ava Duggan <duggan.av@northeastern.edu>
2023-04-13 18:44:54 -0400

    ingredient

--- connector.py
+++ connector.py
@@ -79,16 +79,17 @@ if __name__ == '__main__':
     dbname = get_database()
     collection = dbname['recipes']
 
-    df = run_pca(collection)
+    #df = run_pca(collection)
 
     with open('data/recipe_data.json') as data_file:
         data = json.load(data_file)
 
     collection.insert_many([item for item in data])
+    df = run_pca(collection)
 
     uri = 'bolt://localhost:7687'
     user = 'neo4j'
-    password = 'epd9htf5kvd_hwt.PZR'
+    password = 'Cheetah871' #epd9htf5kvd_hwt.PZR'
     driver = GraphDatabase.driver(uri, auth=(user, password))
 
     # query data from Mongodb
@@ -97,17 +98,20 @@ if __name__ == '__main__':
     with driver.session() as session:
         tx = session.begin_transaction()
         for record in data1:
+            ingredients = []
+            for ingredient in record['ingredients']:
+                ingredients.append(ingredient['name'])
             fields = {'field1': record['name'], 'field2': record['url'], 'field3': record['recipeType'], 'field4':
                 record['keywords'], 'field5': record['description'],
-                      'field6': record['steps']}  # 'field7': [x['name'] for x in record['ingredients']]}
+                      'field6': record['steps'], 'field7': list(filter(None,ingredients))}  # 'field7': [x['name'] for x in record['ingredients']]}
             query = 'CREATE (recipe:recipes {name: $field1, url: $field2, recipeType: $field3, keywords: \
-            $field4, description: $field5, steps: $field6})'  # ingredients: $field7})'
+            $field4, description: $field5, steps: $field6, ingredients: $field7})'
+
+            query2 = 'MERGE (recipe: recipes {ingredients: $list})'
 
             tx.run(query, **fields)
-            # tx.run('UNWIND $value as ingredients  \
-            #         MERGE (recipe:recipes {ing_name:ingredients.name})', value=record['ingredients'])
+            #tx.run('MERGE (recipe: recipes {ingredients: $list1})', list1=list(filter(None,ingredients)))
             # issue with ingredients and tags, reviews, nutrition
-
         tx.commit()
         # for record in data1:
 
